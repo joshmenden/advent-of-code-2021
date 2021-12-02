e130b50873c3129bd7f9199db1428d057b6a6bfd
Josh Menden <josh.menden@gmail.com>
2021-12-01 22:10:35 -0700

    Task 1 complete (day 2)

--- day-2/main.go
+++ day-2/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Instruction struct {
+	Direction string
+	Distance int
+}
 
 func main()  {
-	fmt.Println("hello world")
+	dat, _ := os.ReadFile("./input.txt")
+	lines := strings.Split(string(dat), "\n")
+	var instructions []Instruction
+
+	for _, line := range lines {
+		data := strings.Split(line, " ")
+		dist, _ := strconv.Atoi(data[1])
+		instructions = append(instructions, Instruction{
+			Direction: data[0],
+			Distance:  dist,
+		})
+	}
+
+	horiz := 0
+	vert := 0
+
+	for _, instruction := range instructions {
+		if instruction.Direction == "forward" {
+			horiz += instruction.Distance
+		} else if (instruction.Direction == "down") {
+			vert += instruction.Distance
+		} else if (instruction.Direction == "up") {
+			vert -= instruction.Distance
+		}
+	}
+
+	fmt.Printf("Horizontal: %v, Vertical %v, product of two: %v\n", horiz, vert, (horiz * vert))
 }
