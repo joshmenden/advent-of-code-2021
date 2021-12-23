62425885dfda42632f38f09e62afded5686bd4c4
Josh Menden <josh.menden@gmail.com>
2021-12-23 16:34:30 -0700

    6-1 complete, more to go

--- day-6/main.rb
+++ day-6/main.rb
@@ -1,4 +1,36 @@
 require "byebug"
-fname = "input.txt"
+fname = "example.txt"
+
+
+class LanternFish
+  attr_accessor :timer
+
+  def initialize(timer:)
+    @timer = timer
+  end
+end
+
+fishes = []
 
 content = File.read(fname)
+content.split(",").each do |age|
+  fishes.push(LanternFish.new(timer: age.to_i))
+end
+
+for day in 1..256 do
+  # puts fishes.map(&:timer).join(" ")
+
+  new_fish = []
+  fishes.each do |fish|
+    if fish.timer == 0
+      new_fish.push(LanternFish.new(timer: 8))
+      fish.timer = 6
+    else
+      fish.timer -= 1
+    end
+  end
+
+  fishes = fishes.concat(new_fish)
+end
+
+puts fishes.count
