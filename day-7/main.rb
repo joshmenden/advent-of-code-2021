eda85bec3b411900c8a36cf8c2d2c71be0a64545
Josh Menden <josh.menden@gmail.com>
2021-12-23 17:19:54 -0700

    Day 7 complete with memoization

--- day-7/main.rb
+++ day-7/main.rb
@@ -1,3 +1,35 @@
 require "byebug"
-fname = "example.txt"
+fname = "input.txt"
 content = File.read(fname)
+positions = content.split(",").map { |pos| pos.to_i }
+
+@costs_map = {}
+
+def calculate_fuel(pos, home)
+  abs = (pos - home).abs()
+  return @costs_map[abs] if @costs_map.key?(abs)
+
+  stepper = 1
+  fuel = 0
+  for pos in 1..(abs) do
+    fuel += stepper
+    stepper += 1
+  end
+
+  @costs_map[abs] = fuel
+
+  return fuel
+end
+
+
+min = positions.min
+max = positions.max
+
+fuel_costs = []
+
+for homebase in min..max do
+  cost = positions.map { |pos| calculate_fuel(pos, homebase) }.reduce(:+)
+  fuel_costs.push(cost)
+end
+
+puts fuel_costs.min
